353e91d3707334665eeba2f3c917b9c4cf7c22b6
Ліля <chlvskalilia@gmail.com>
2023-11-05 18:37:52 +0200

    first test

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,26 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab5.2/lab5.2.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double x = 0.5;
+			double eps = 0.0000001;
+			int n = 0;
+			double s = 0;
+			double expected = atan(x);
+
+			double actual = S(x, eps, n, s);
+
+			Assert::AreEqual(expected, actual, eps);
+		}
+	};
+}
